1861ead281a891158cb9d5b3f3fb7b50d6a9916c
ibll <61601241+ibll@users.noreply.github.com>
2025-09-04 11:32:27 -0500

    Groundwork for constrained movement

--- scenes/piece/piece.gd
+++ scenes/piece/piece.gd
@@ -1,25 +1,34 @@
 extends Node2D
+class_name Piece
 
 @export var player := 0
-@export var spriteIndex := 0
-@export var squarePos: Vector2i
+@export var sprite_index := 0
+@export var square_pos: Vector2i
 
 
 func _ready():
-	setSprite(spriteIndex);
+	setSprite(sprite_index);
 
 
 func loadPieceData(data):
 	player = data.player
-	spriteIndex = data.spriteIndex
+	sprite_index = data.spriteIndex
 	setSquarePos(Vector2i(data.x, data.y))
 
 
 func setSprite(sprite: int):
-	spriteIndex = sprite
-	$Sprite.frame = spriteIndex + player * 32
+	sprite_index = sprite
+	$Sprite.frame = sprite_index + player * 32
 
 
 func setSquarePos(pos: Vector2i):
-	squarePos = pos
+	square_pos = pos
 	set_position(Vector2((pos.x + 1) * 16 - 8, (pos.y) * 16 + 4))
+
+
+func canMoveTo(pos: Vector2i):
+	if square_pos == pos: return false
+
+	if pos.x == square_pos.x: return true
+	if pos.y == square_pos.y: return true
+	return false
