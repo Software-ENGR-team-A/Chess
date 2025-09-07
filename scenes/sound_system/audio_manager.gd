a4137ba4c6e7860fe5b359d0f350a4b688dd9342
Aaron Massey <03masseyaaron@gmail.com>
2025-09-07 16:40:47 -0500

    Improved musicplayer

--- scenes/sound_system/audio_manager.gd
+++ scenes/sound_system/audio_manager.gd
@@ -12,9 +12,13 @@ func _ready():
 	sfx_bus_idx = AudioServer.get_bus_index(bus_name)
 
 
+## Plays the desired sound effect from the [MenuSounds] or [MovementSounds]
+## Resource file
+## [param stream] Is the audiostream from the resource
+## [param volume_db] is the dB level for the audioStream
 func play_sound(stream: AudioStream, volume_db: float = 0.0) -> void:
 	if not stream:
-		printerr("Attempted to play a null audio stream.")
+		printerr("AudioManager: Attempted to play a null audio stream.")
 		return
 
 	var player = AudioStreamPlayer.new()
@@ -29,16 +33,20 @@ func play_sound(stream: AudioStream, volume_db: float = 0.0) -> void:
 	player.finished.connect(player.queue_free)
 
 
+## Sets the [member volume_db] property of the SFX bus
+## [param vol_db] The dB volume level desired
 func set_volume(vol_db: float) -> void:
 	if sfx_bus_idx == -1:  # If the bus is not found, give an error
-		printerr("Audio bus '", bus_name, "' not found. Could not set volume.")
+		printerr("AudioManager: Audio bus '", bus_name, "' not found. Could not set volume.")
 		return
 	# Otherwise set the volume
 	AudioServer.set_bus_volume_db(sfx_bus_idx, vol_db)
 
 
+## Returns the current SFX bus volume
+## Returns -1000 if the bus is not found
 func get_volume() -> float:
 	if sfx_bus_idx == -1:  # If the bus is not found, give an error
-		printerr("Audio bus '", bus_name, "' not found. Could not set volume.")
+		printerr("AudioManager: Audio bus '", bus_name, "' not found. Could not set volume.")
 		return -1000
 	return AudioServer.get_bus_volume_db(sfx_bus_idx)
---
+++ scenes/sound_system/sound_groups/MusicLibrary.gd
@@ -0,0 +1,5 @@
+class_name MusicLibrary
+extends Resource
+
+@export var main_menu_tracks: Array[AudioStream]
+@export var gameplay_tracks: Array[AudioStream]
