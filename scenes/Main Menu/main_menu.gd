811ef09c3d6eb7dd15d52eba2c3494c139c74856
PancakeGremory <benjamin.g.gattis-1@ou.edu>
2025-09-07 13:34:45 -0500

    Added basic start menu

---
+++ scenes/Main Menu/main_menu.gd
@@ -0,0 +1,19 @@
+class_name MainMenu
+extends Control
+
+
+@onready var start_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/Start_Button as Button
+@onready var exit_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/Exit_Button as Button
+@onready var Chess_TwoPlayer = preload("res://scenes/board/board.tscn") as PackedScene
+
+func _ready():
+	start_button.button_down.connect(on_start_pressed)
+	exit_button.button_down.connect(on_exit_pressed)
+
+
+func on_start_pressed() -> void:
+	get_tree().change_scene_to_packed(Chess_TwoPlayer)
+
+
+func on_exit_pressed() -> void:
+	get_tree().quit()
