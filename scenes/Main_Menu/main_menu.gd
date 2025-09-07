681d4704f3cceb1c440bf3d1eefcedcf835264da
PancakeGremory <benjamin.g.gattis-1@ou.edu>
2025-09-07 18:30:24 -0500

    Hopfully this actually works this time

--- scenes/Main_Menu/main_menu.gd
+++ scenes/Main_Menu/main_menu.gd
@@ -1,10 +1,11 @@
 class_name MainMenu
 extends Control
 
-
-@onready var start_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/Start_Button as Button
+@onready
+var start_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/Start_Button as Button
 @onready var exit_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/Exit_Button as Button
 @onready var Chess_TwoPlayer = preload("res://scenes/board/board.tscn") as PackedScene
+
 
 func _ready():
 	start_button.button_down.connect(on_start_pressed)
