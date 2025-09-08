b579911191de4e64915129aff130af9141d19670
PancakeGremory <benjamin.g.gattis-1@ou.edu>
2025-09-07 21:45:58 -0500

    Added music to main menu, Added conformation popup on exit

--- scenes/Main_Menu/main_menu.gd
+++ scenes/Main_Menu/main_menu.gd
@@ -1,20 +1,37 @@
 class_name MainMenu
 extends Control
 
-@onready
-var start_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/Start_Button as Button
-@onready var exit_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/Exit_Button as Button
-@onready var Chess_TwoPlayer = preload("res://scenes/board/board.tscn") as PackedScene
+@export var start_button: Button
+@export var exit_button: Button
+@export var yes_button: Button
+@export var no_button: Button
+@onready var chess_two_player = preload("res://scenes/board/board.tscn") as PackedScene
 
 
 func _ready():
+	MusicManager.play_random_song()
+	MusicManager.set_volume(1)
+	print(start_button)
 	start_button.button_down.connect(on_start_pressed)
 	exit_button.button_down.connect(on_exit_pressed)
+	yes_button.button_down.connect(on_yes_pressed)
+	no_button.button_down.connect(on_no_pressed)
 
 
 func on_start_pressed() -> void:
-	get_tree().change_scene_to_packed(Chess_TwoPlayer)
+	MusicManager.stop_music()
+	AudioManager.play_selectmenu()
+	await get_tree().create_timer(.5).timeout
+	get_tree().change_scene_to_packed(chess_two_player)
 
 
 func on_exit_pressed() -> void:
+	%Conformation_Box.popup()
+
+
+func on_yes_pressed() -> void:
 	get_tree().quit()
+
+
+func on_no_pressed() -> void:
+	%Conformation_Box.hide()
