efd0d4b87b9f8d225f0497d84aa394e9bda9e217
PancakeGremory <benjamin.g.gattis-1@ou.edu>
2025-12-11 22:13:11 -0600

    gdlint says there are no problems this time

--- src/test/scenes/menus/main_menu_test/main_menu_test.gd
+++ src/test/scenes/menus/main_menu_test/main_menu_test.gd
@@ -4,16 +4,13 @@ extends GdUnitTestSuite
 @warning_ignore("unused_parameter")
 @warning_ignore("return_value_discarded")
 # TestSuite generated from
-const __source: String = "res://scenes/menus/main_menu/main_menu.gd"
-
-const __source_scene = "res://scenes/menus/main_menu/main_menu.tscn"
+const SOURCE_SCENE := preload("res://scenes/menus/main_menu/main_menu.gd")
 
 var main_menu: Node
 
 
 func before_test() -> void:
-	# Instantiate the scene
-	main_menu = preload(__source_scene).instantiate()
+	main_menu = SOURCE_SCENE.instantiate()
 	get_tree().root.add_child(main_menu)
 
 
@@ -22,7 +19,7 @@ func after_test() -> void:
 		main_menu.queue_free()
 
 
-func test__ready() -> void:
+func test_ready() -> void:
 	# Call _ready() to ensure it runs without errors
 	main_menu._ready()
 
