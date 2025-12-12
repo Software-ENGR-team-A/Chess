b74fe702cdf177e8385dcb23495bcff039304d81
ibll <61601241+ibll@users.noreply.github.com>
2025-12-11 22:25:14 -0600

    Add back to menu from pause

--- src/scenes/menus/pause_menu/pause_menu.gd
+++ src/scenes/menus/pause_menu/pause_menu.gd
@@ -2,9 +2,16 @@ extends Control
 
 @onready var board = $"../"
 
+const MAIN_SCENE := preload("res://scenes/menus/main_menu/main_menu.tscn")
+
 
 func _on_resume_pressed() -> void:
 	board.pause()
+
+
+func _on_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/menus/main_menu/main_menu.tscn")
+	board.queue_free()
 
 
 func _on_quit_pressed() -> void:
